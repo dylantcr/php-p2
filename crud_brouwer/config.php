85194ca845f582c28e896fefc3eb1d8602379d4c
dylantcr <9021142@student.zadkine.nl>
2024-03-05 10:16:06 +0100

    change

---
+++ crud_brouwer/config.php
@@ -0,0 +1,13 @@
+<?php
+// auteur: Dylan
+// functie: configuratiebestand
+
+//namen geven
+define("DATABASE", "brouwer");
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+
+define("CRUD_TABLE", "brouwer");
+
+?>
